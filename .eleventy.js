fa355db19b0474ec31bd0086dbfded7d8293eac0
Matthias Baetens <baetensmatthias@gmail.com>
2025-10-31 18:33:05 +0800

    Remove old config

--- .eleventy.js
+++
@@ -1,3 +0,0 @@
-// Eleventy v3 prefers `eleventy.config.cjs` or `eleventy.config.js`.
-// Keep this file for backward compatibility and delegate to the new config.
-module.exports = require("./eleventy.config.cjs");
